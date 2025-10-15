5e7d86f938cfa4f2551c52c98778341336828a00
Azeemhaleem <157594820+Azeemhaleem@users.noreply.github.com>
2025-10-15 23:22:22 +0530

    Add some club coding's

--- frontend/src/Components-Club/ClubAds.jsx
+++ frontend/src/Components-Club/ClubAds.jsx
@@ -1,9 +1,23 @@
-
+import CreateAds from '../assets/createAds.png'
 
 export default function ClubAds(){
     return (
         <div>
-            Club Ads
+            <h2 className="text-3xl font-bold m-2">Club Ads</h2>
+            <div className="w-9/10 mx-auto items-center flex bg-blue-200 my-5 rounded-2xl">
+                <div className="block w-1/2 p-4 mx-auto">
+                    <h2 className="text-2xl font-bold mb-4">Create Impactful Ads, Effortlessly</h2>
+                    <p className="text-xl">Reach your audience effectively with Uniplaza.
+                        Design and launch Event Ads or general Announcements with ease,
+                        all from one powerful platform.</p>
+                </div>
+                <div className="flex justify-end items-center mr-10 p-10 w-1/3">
+                    <img src={CreateAds} alt="" className="w-2/3"/>
+                </div>
+            </div>
+            <div>
+                
+            </div>
         </div>
     )
 }
--- frontend/src/Components-Club/ClubDashboard.jsx
+++ frontend/src/Components-Club/ClubDashboard.jsx
@@ -1,9 +1,49 @@
+import AdImage from '../assets/adImage.png'
 
 
 export default function ClubDashboard(){
     return (
         <div>
-            Club Dashboard
+            <h2 className="text-3xl font-bold m-2">Club Dashboard</h2>
+            <div className="w-9/10 mx-auto my-5 border-1 border-gray-300 rounded-2xl">
+                <div className="w-9/10 mx-auto">
+                    <div className="my-4">
+                        <h2 className="text-2xl font-bold mb-2">Current Active Ads</h2>
+                        <p className="text-xl">Monitor and manage your club's active advertisements.</p>
+                    </div>
+                    <div className="block md:flex my-4 bg-gray-200 rounded-2xl p-4">
+                        <img src={AdImage} alt="" className="rounded-2xl mx-auto"/>
+                        <div className="block mx-4 my-4">
+                            <h2 className="text-lg font-bold">Holy Festival-2025</h2>
+                            <p>Join us for an unforgettable evening of music, dance, and cultural performances
+                                celebrating the arrival of holy! Enjoy diverse acts from various student groups and
+                                indulge in delicious food from local vendors. Mark your calendars!</p>
+                        </div>
+                        <div className="flex items-center justify-end">
+                            <button className="bg-buttonBlue text-white rounded-2xl py-2 px-4">
+                                Edit
+                            </button>
+                        </div>
+                    </div>
+                    <div className="block md:flex my-4 bg-gray-200 rounded-2xl p-4">
+                        <img src={AdImage} alt="" className="rounded-2xl mx-auto"/>
+                        <div className="block mx-4 my-4">
+                            <h2 className="text-lg font-bold">Holy Festival-2025</h2>
+                            <p>Join us for an unforgettable evening of music, dance, and cultural performances
+                                celebrating the arrival of holy! Enjoy diverse acts from various student groups and
+                                indulge in delicious food from local vendors. Mark your calendars!</p>
+                        </div>
+                        <div className="flex items-center justify-end">
+                            <button className="bg-buttonBlue text-white rounded-2xl py-2 px-4">
+                                Edit
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex justify-end my-4 ">
+                        <button className="border-2 border-gray-400 text-gray-500 rounded-2xl py-2 px-4">View All</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

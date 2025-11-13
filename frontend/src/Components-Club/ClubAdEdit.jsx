7a5aada995c2b7c1d75d3454c96379266e7d7ad9
Azeemhaleem <157594820+Azeemhaleem@users.noreply.github.com>
2025-11-13 10:38:11 +0530

    update routing and codes

---
+++ frontend/src/Components-Club/ClubAdEdit.jsx
@@ -0,0 +1,132 @@
+import CreateAds from "../assets/createAds.png";
+import {useState} from "react";
+
+
+export default function ClubAdEdit() {
+    const [file,setFile] = useState(null);
+    const [input,setInput] = useState("");
+    const [tags,setTags] = useState([]);
+    const [active,setActive] = useState(false);
+    const [form,setForm] = useState(true);
+
+    const handleKeyDown = (e) => {
+        if(e.key === "Tab" || e.key === ","){
+            e.preventDefault();
+            setActive(true);
+            const newTags = input.trim().toLocaleUpperCase();
+            if(newTags && !tags.includes(newTags)){
+                setTags([...tags,newTags]);
+            }
+            setInput("");
+        }
+    };
+
+    const handleForm = () =>{
+        setForm(!form);
+    }
+
+    const removeTags = (tagToRemove) => {
+        setTags(tags.filter((tag) => tag !== tagToRemove));
+    }
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    return (
+        <div className="pb-10 my-4">
+            <div className="mt-8 mb-4 w-92/100 mx-auto text-center md:text-left">
+                <h2 className="mb-2 text-2xl md:text-3xl font-bold">Edit Event</h2>
+                <p className="text-md text-gray-500">Update the details for the event and save the changes</p>
+            </div>
+
+            <div className="w-92/100 mx-auto">
+                <div className="border border-gray-400 rounded-2xl mb-10">
+                    <div className="w-9/10 mx-auto my-5">
+                        <div className="my-5">
+                            <h2 className="text-lg md:text-xl font-bold">Create Event Ad</h2>
+                            <p className="text-base md:text-lg mb-5">Fill in the details for your event advertisement.</p>
+                            <form action="">
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-5">
+                                    <div className="flex flex-col">
+                                        <label className='flex'>Event Title<span className='text-red-600 text-xl'>*</span></label>
+                                        <input type="text" placeholder="Annual Tech Summit 2024" className="p-2 bg-gray-200 rounded-xl"/>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='flex'>Event Date<span className='text-red-600 text-xl'>*</span></label>
+                                        <input type="text" placeholder="October 26, 2024" className="p-2 bg-gray-200 rounded-xl"/>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='flex'>Start Time<span className='text-red-600 text-xl'>*</span></label>
+                                        <input type="text" placeholder="10.00 AM" className="p-2 bg-gray-200 rounded-xl"/>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className='flex'>Location / Venue<span className='text-red-600 text-xl'>*</span></label>
+                                        <input type="text" placeholder="Grand Convention Hall" className="p-2 bg-gray-200 rounded-xl mb-3"/>
+                                        <input type="text" placeholder="https://maps.app.goo.gl/example" className="p-2 bg-gray-200 rounded-xl"/>
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col mb-5">
+                                    <label className='flex'>University Name<span className='text-red-600 text-xl'>*</span></label>
+                                    <input type="text" placeholder="University of Kelaniya" className="p-2 bg-gray-200 rounded-xl"/>
+                                </div>
+
+                                <div className="flex flex-col my-5">
+                                    <label className='flex'>Description of the event<span className='text-red-600 text-xl'>*</span></label>
+                                    <textarea className="p-2 bg-gray-200 rounded-xl" rows="3" placeholder="Join us for the most anticipated technology event of the year! Explore cutting-edge innovations, network with industry leaders, and discover the future of tech."/>
+                                </div>
+
+                                <div>
+                                    <h2 className="mb-5">Related Links (optional)</h2>
+                                    <div className="flex flex-col md:flex-row mb-5 w-full gap-4">
+                                        <div className="border border-dashed border-gray-400 p-2 rounded-2xl text-center md:w-1/3 w-full">Register Now</div>
+                                        <div className="w-full md:w-2/3">
+                                            <input type="text" placeholder="https://techsummit24.com/register" className="p-2 bg-gray-200 rounded-xl w-full"/>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex flex-col md:flex-row mb-5 w-full gap-4">
+                                        <div className="flex flex-col md:w-1/3 w-full">
+                                            <label className='flex'>Contact number<span className='text-red-600 text-xl'>*</span></label>
+                                            <input type="text" placeholder="073 923404-98" className="p-2 bg-gray-200 rounded-xl w-full"/>
+                                        </div>
+
+                                        <div className="flex flex-col md:w-2/3 w-full">
+                                            <label className='flex'>Upload Poster/Image(s)<span className='text-red-600 text-xl'>*</span></label>
+                                            <label htmlFor="poster" className="block border-2 border-dashed border-gray-300 bg-gray-200 rounded-2xl p-6 text-center text-gray-500 text-sm cursor-pointer hover:bg-gray-100">
+                                                Drag and drop your event poster here, or click to browse (Max 5MB)
+                                            </label>
+                                            <input id="poster" type="file" accept="image/*" className="hidden" onChange={handleFileChange} />
+                                            {file && <p className="text-gray-600 mt-2">Selected file: {file.name}</p>}
+                                        </div>
+                                    </div>
+
+                                    <div className="my-5">
+                                        <div className="flex flex-col mb-5">
+                                            <label>Email Address</label>
+                                            <input type="text" placeholder="club@example.edu" className="p-2 bg-gray-200 rounded-xl"/>
+                                        </div>
+                                        <div className="flex flex-col mb-8">
+                                            <label>Tags</label>
+                                            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type and press Tab..." onKeyDown={handleKeyDown} className="p-2 bg-gray-200 rounded-xl"/>
+                                            <div className={`flex flex-wrap items-center gap-2 mt-3 ${active? 'border border-gray-300 rounded-md px-3 py-2 bg-blue-50 focus-within:ring-1 focus-within:ring-blue-400':'border-0 bg-white'}`}>
+                                                {tags.map((tag, index) => (
+                                                    <div key={index} className="flex items-center gap-1 bg-white border border-gray-300 rounded-full px-3 py-1 text-sm text-blue-600">
+                                                        <span>{tag}</span>
+                                                        <button type="button" onClick={() => removeTags(tag)} className="text-gray-500 hover:text-red-500">×</button>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="bg-buttonBlue w-full text-center text-white py-3 rounded-2xl">Submit for Approval</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
